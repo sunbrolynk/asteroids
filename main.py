9651fa3316e64f9a6aad37294bc8d717640b1fc0
sunbrolynk <swagar003@gmail.com>
2025-09-19 19:54:29 -0600

    added circleshape.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import modules from other files
 import pygame
 from constants import *
 
@@ -5,13 +6,20 @@ def main():
     print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
-
+        
+        screen.fill("black")
+        pygame.display.flip()
+    
+    
+        #  limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
