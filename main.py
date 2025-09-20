3bfe68160cc21407084e79fa1b12bfa908b94474
sunbrolynk <swagar003@gmail.com>
2025-09-19 19:44:26 -0600

    Game Loop

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
+from constants import *
 
 def main():
-    print()
+    print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.fill("black")
+    pygame.display.flip()
+
 
 
 if __name__ == "__main__":
